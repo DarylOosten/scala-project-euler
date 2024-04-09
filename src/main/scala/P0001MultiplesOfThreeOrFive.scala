8968bb087e8ea68194f1b761529f589bf15d3257
aoi <me@darylium.net>
2024-04-09 13:55:41 +0700

    Implement iterative version

--- src/main/scala/P0001MultiplesOfThreeOrFive.scala
+++ src/main/scala/P0001MultiplesOfThreeOrFive.scala
@@ -1,12 +1,23 @@
 object P0001MultiplesOfThreeOrFive {
   def main(args: Array[String]): Unit = {
-    def multiple(x: Int, y: Int, z: Int): Long = {
+    def recursive(x: Int, y: Int, z: Int): Long = {
       z match {
         case n if n == 0 => z
-        case n if n % x == 0 || n % y == 0 => z + multiple(x, y, z - 1)
-        case _ => multiple(x, y, z - 1)
+        case n if n % x == 0 || n % y == 0 => z + recursive(x, y, z - 1)
+        case _ => recursive(x, y, z - 1)
       }
     }
-    println(multiple(3, 5, 1000))
+    println(recursive(3, 5, 1000))
+
+    def iterative(x: Int, y: Int, z: Int): Long = {
+      var sum = 0
+      for(i <- 0 until z) {
+        if(i % x == 0 || i % y == 0) {
+          sum = sum + i
+        }
+      }
+      sum
+    }
+    println(iterative(3, 5, 1000))
   }
 }
